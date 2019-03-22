ced2d410208afdbb878c709a97b64d1ab691d62e
lumen <18733690637@163.com>
2019-03-22 10:53:25 +0000

    aa

--- app/Http/Controllers/Port/PortController.php
+++ app/Http/Controllers/Port/PortController.php
@@ -72,7 +72,7 @@ class PortController extends Controller
             $token = substr(md5(time().mt_rand(1,99999)),10,10);
             setcookie('uid',$info->uid,time()+86400,'/','hz4155.cn',false,true);
             setcookie('token',$token,time()+86400,'/','hz4155.cn',false,true);
-            var_dump($_COOKIE);die;
+            var_dump($_COOKIE);
             $redis_key="h:u:s".$info->id;
             Redis::set($redis_key,$token);
             Redis::expire($redis_key,86400);
@@ -102,8 +102,8 @@ class PortController extends Controller
         if($res){
             if(password_verify($pwd,$res->pwd)){
                 $token=substr(md5(time().mt_rand(1,9999)),10,10);
-                setcookie('token',$token,time()+86400,'/','shop.com',false,true);
-                setcookie('id',$res->id,time()+86400,'/','shop.com',false,true);
+                setcookie('token',$token,time()+86400,'/','hz4155.cn',false,true);
+                setcookie('id',$res->id,time()+86400,'/','hz4155.cn',false,true);
                 //var_dump($_COOKIE);die;
                 //存redis
                 $redis_key="h:u:s".$res->id;
